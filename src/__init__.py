b64cadc371c0e2645b9e8680b579e4fea78e04c2
jacqle <jacqmin.leo@gmail.com>
2021-06-18 21:30:05 +0200

    merge changes

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .gector import model
-import re
-
-model = model.load_model(
-    vocab_path = "src/gector/data/output_vocabulary",
-    model_paths = ["src/gector/data/model_files/xlnet_0_gector.th"],
-    model_name = "xlnet"
-)
